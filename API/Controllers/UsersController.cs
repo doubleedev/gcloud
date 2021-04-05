bdc6b03152f5559d497ba29bb844ad70a82d0f4f
kevin.trasler <62543202+ktrasler@users.noreply.github.com>
2021-04-05 23:39:23 +0100

    docker

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,24 +1,26 @@
 using API.BusinessLogic;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-	[ApiController]
-	[Route("api/[controller]")]
-	public class UsersController : Controller
-	{
-		private readonly IUserRepository _UserRepository;
+    [AllowAnonymous]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        private readonly IUserRepository _UserRepository;
 
-		public UsersController(IUserRepository userRepository)
-		{
-			_UserRepository = userRepository;
-		}
+        public UsersController(IUserRepository userRepository)
+        {
+            _UserRepository = userRepository;
+        }
 
-		[HttpGet]
-		public IActionResult GetAll()
-		{
-			var users = _UserRepository.GetAll();
-			return Ok(users);
-		}
-	}
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _UserRepository.GetAll();
+            return Ok(users);
+        }
+    }
 }
