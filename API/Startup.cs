7f0c12c1efc2d65cb5381e50c92df6658607866c
kevin.trasler <62543202+ktrasler@users.noreply.github.com>
2021-04-07 09:48:13 +0100

    sql connection

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using API.BusinessLogic;
@@ -52,14 +53,17 @@ namespace API
                 });
             });
 
-            var server = "35.242.156.191"; // public ip
-            var name = "doublee-sample-20210405:europe-west2:doublee-sql"; // not using
+            // var server = "35.242.156.191"; // public ip
+            // var name = "doublee-sample-20210405:europe-west2:doublee-sql"; // not using
 
-            var database = "sample";
-            var username = "sqlserver";
-            var password = "sogismhI0P2aM6kl";
+            // var database = "sample";
+            // var username = "sqlserver";
+            // var password = "sogismhI0P2aM6kl";
 
-            var connString = $@"Server={server};Initial Catalog={database};Integrated Security=false;uid={username};password={password};";
+            // var connectionString = $@"Server={server};Initial Catalog={database};Integrated Security=false;uid={username};password={password};";
+
+            var connectionString = GetSqlServerConnectionString();
+            var connString = connectionString.ConnectionString;
 
             Configuration["ConnectionStrings:DefaultConnection"] = connString;
 
@@ -100,6 +104,51 @@ namespace API
                 endpoints.MapControllers();
                 endpoints.MapFallbackToController("Index", "Fallback");
             });
+        }
+
+        public static SqlConnectionStringBuilder GetSqlServerConnectionString()
+        {
+            // [START cloud_sql_sqlserver_dotnet_ado_connection_tcp]
+            // Equivalent connection string:
+            // "User Id=<DB_USER>;Password=<DB_PASS>;Server=<DB_HOST>;Database=<DB_NAME>;"
+            var connectionString = new SqlConnectionStringBuilder()
+            {
+                // Remember - storing secrets in plain text is potentially unsafe. Consider using
+                // something like https://cloud.google.com/secret-manager/docs/overview to help keep
+                // secrets secret.
+                DataSource = Environment.GetEnvironmentVariable("DB_HOST"),     // e.g. '127.0.0.1'
+                // Set Host to 'cloudsql' when deploying to App Engine Flexible environment
+                UserID = Environment.GetEnvironmentVariable("DB_USER"),         // e.g. 'my-db-user'
+                Password = Environment.GetEnvironmentVariable("DB_PASS"),       // e.g. 'my-db-password'
+                InitialCatalog = Environment.GetEnvironmentVariable("DB_NAME"), // e.g. 'my-database'
+
+                // The Cloud SQL proxy provides encryption between the proxy and instance
+                Encrypt = false,
+            };
+            connectionString.Pooling = true;
+            // [START_EXCLUDE]
+            // The values set here are for demonstration purposes only. You 
+            // should set these values to what works best for your application.
+            // [START cloud_sql_sqlserver_dotnet_ado_limit]
+            // MaximumPoolSize sets maximum number of connections allowed in the pool.
+            connectionString.MaxPoolSize = 5;
+            // MinimumPoolSize sets the minimum number of connections in the pool.
+            connectionString.MinPoolSize = 0;
+            // [END cloud_sql_sqlserver_dotnet_ado_limit]
+            // [START cloud_sql_sqlserver_dotnet_ado_timeout]
+            // ConnectionTimeout sets the time to wait (in seconds) while
+            // trying to establish a connection before terminating the attempt.
+            connectionString.ConnectTimeout = 15;
+            // [END cloud_sql_sqlserver_dotnet_ado_timeout]
+            // [START cloud_sql_sqlserver_dotnet_ado_lifetime]
+            // ADO.NET connection pooler removes a connection
+            // from the pool after it's been idle for approximately
+            // 4-8 minutes, or if the pooler detects that the
+            // connection with the server no longer exists.
+            // [END cloud_sql_sqlserver_dotnet_ado_lifetime]
+            // [END_EXCLUDE]
+            return connectionString;
+            // [END cloud_sql_sqlserver_dotnet_ado_connection_tcp]
         }
     }
 }
